59352394e3dab3f05989bfdef57e5c55c4bcf6f8
gazorpazorpius <edcussmusic@gmail.com>
2024-10-01 21:58:12 +0100

    style: detekt fixes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,25 +6,25 @@ fun main() {
 class Game(
     private val rnd: (List<String>) -> String = { it.random() }
 ) {
-   operator fun invoke() {
-       val choices = listOf("rock", "paper", "scissors")
-       val compChoice = rnd(choices)
-       println("Enter choice:")
-       val userIn = readln()
-       if(userIn !in choices) {
-           println("Invalid input: $userIn")
-           return
-       }
-       println("Computer choice: $compChoice")
-       if(compChoice == userIn) {
-           println("draw")
-           return
-       }
-       when("$compChoice $userIn") {
-           "rock paper" -> println("win")
-           "paper scissors" -> println("win")
-           "scissors rock" -> println("win")
-           else -> println("lose")
-       }
-   }
+    operator fun invoke() {
+        val choices = listOf("rock", "paper", "scissors")
+        val compChoice = rnd(choices)
+        println("Enter choice:")
+        val userIn = readln()
+        if (userIn !in choices) {
+            println("Invalid input: $userIn")
+            return
+        }
+        println("Computer choice: $compChoice")
+        if (compChoice == userIn) {
+            println("draw")
+            return
+        }
+        when ("$compChoice $userIn") {
+            "rock paper" -> println("win")
+            "paper scissors" -> println("win")
+            "scissors rock" -> println("win")
+            else -> println("lose")
+        }
+    }
 }
